293380b61c28e3d5c4874a155bad236b3cc34ce8
yanischoucha <115161881+yanischoucha@users.noreply.github.com>
2022-10-06 15:31:56 +0200

    Delete .github/workflows directory

--- .github/workflows/Portfolio dev/Dossier bubule/fonctions.js
+++
@@ -1,9 +0,0 @@
-function random(min, max) {
-    return Math.round(Math.random() * (max - min) + min);
-}
-
-document.querySelector(".loader").style.display = "none";
-
-//document.getElementById(id).style.property = new style; 
-    
-console.log("AHGDHJGAH");
--- .github/workflows/Portfolio dev/connexion.php
+++
@@ -1,2 +0,0 @@
-<!-- Connexion à la base de données-->
-<?php $db = new PDO('mysql:host=localhost;dbname=douangpraseuth_expasia;charset=utf8', 'douangpraseuth_expasia', 'motdepasseexpasia'); ?>
