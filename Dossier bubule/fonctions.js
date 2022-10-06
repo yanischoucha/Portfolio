96a09f490e85d6adcc6e21f0cb59feeec7a8fca9
yanischoucha <115161881+yanischoucha@users.noreply.github.com>
2022-10-06 15:36:39 +0200

    Add files via upload

---
+++ Dossier bubule/fonctions.js
@@ -0,0 +1,9 @@
+function random(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+document.querySelector(".loader").style.display = "none";
+
+//document.getElementById(id).style.property = new style; 
+    
+console.log("AHGDHJGAH");
---
+++ connexion.php
@@ -0,0 +1,2 @@
+<!-- Connexion à la base de données-->
+<?php $db = new PDO('mysql:host=localhost;dbname=douangpraseuth_expasia;charset=utf8', 'douangpraseuth_expasia', 'motdepasseexpasia'); ?>
